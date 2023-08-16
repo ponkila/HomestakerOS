6056c30f22d0b25c0bb558aab9a5955ab11387cb
tupakkatapa <jesse@ponkila.com>
2023-08-16 18:43:51 +0300

    json2nix-script: just convert

--- scripts/json2nix.sh
+++ scripts/json2nix.sh
@@ -1,21 +1,7 @@
 #!/usr/bin/env bash
-# Script for creating a 'nixosConfigurations/<hostname>/default.nix' from JSON data
+# Script for converting a JSON data to Nix expression
 
 set -o pipefail
-
-# Check argument count
-if [ $# -ne 1 ]; then
-  echo "Usage: cat foo.json | nix run .#json2nix -- <hostname>"
-  exit 1
-fi
-
-hostname="$1"
-default_nix="nixosConfigurations/$hostname/default.nix"
-
-# Create the $hostname folder if it doesn't exist
-if [ ! -d "$(dirname "$default_nix")" ]; then
-  mkdir -p "$(dirname "$default_nix")"
-fi
 
 # Read JSON data from stdin if available
 if [ -p /dev/stdin ]; then
@@ -34,11 +20,5 @@ esc_json_data="${json_data//\"/\\\"}"
 # Convert JSON to Nix expression
 nix_expr=$(nix-instantiate --eval --expr "builtins.fromJSON \"$esc_json_data\"") || exit 1
 
-# Create default.nix file
-cat > "$default_nix" << EOF
-{ pkgs, config, inputs, lib, ... }:
-{
-  homestakeros = $nix_expr;
-}
-EOF
-
+# Print to stdout
+echo "$nix_expr"
