4572d4e6f745718e69b728d63f177ce6c9432e43
tupakkatapa <jesse@ponkila.com>
2023-08-22 20:24:51 +0300

    webui, api: fix

--- webui/api.js
+++ webui/api.js
@@ -1,11 +1,13 @@
 import express from 'express'
+import fs from 'fs'
 
 const router = express.Router()
+var writable = fs.createWriteStream('pipe')
 
 router.post('/nixosConfig', (req, res) => {
   const homestakerConfig = req.body.homestakeros
   const hostname = homestakerConfig.localization.hostname
-  console.log(`echo '${JSON.stringify(homestakerConfig)}' | nix run .#buidl -- -n '${hostname}' -b homestakeros`)
+  writable.write(`echo '${JSON.stringify(homestakerConfig)}' | nix run .#buidl -- -n '${hostname}' -b homestakeros`)
   res.json({ status: 'ok' })
 })
 
