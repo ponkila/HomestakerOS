3a0d6d6c02058e6902a7222ce0906c9ba62cdb82
tupakkatapa <jesse@ponkila.com>
2023-08-22 21:43:10 +0300

    mission-control, server: rm chmod

--- flake.nix
+++ flake.nix
@@ -67,7 +67,6 @@
             description = "Initialize and launch the web server";
             exec = ''
               nix eval --json .#schema | jq > webui/schema.json \
-              && chmod +x app.js \
               && nix run .#
             '';
             category = "Essentials";
