38abb1c28e96d9bb803103597c233908e3571fa3
tupakkatapa <jesse@ponkila.com>
2023-08-23 04:31:51 +0300

    webui, api: add realize flag

--- webui/api.js
+++ webui/api.js
@@ -7,7 +7,7 @@ var writable = fs.createWriteStream('pipe')
 router.post('/nixosConfig', (req, res) => {
   const homestakerConfig = req.body.homestakeros
   const hostname = homestakerConfig.localization.hostname
-  writable.write(`echo '${JSON.stringify(homestakerConfig)}' | nix run .#buidl -- -n '${hostname}' -b homestakeros\n`)
+  writable.write(`echo '${JSON.stringify(homestakerConfig)}' | nix run .#buidl -- -n '${hostname}' -b homestakeros -r\n`)
   res.json({ status: 'ok' })
 })
 
