3909a1bf1ae53f06d7837dc38851d19f853e2848
tupakkatapa <jesse@ponkila.com>
2023-08-31 20:24:42 +0300

    app.js: update paths

--- packages/backend/app.js
+++ packages/backend/app.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 import express from "express";
-import apiRouter from "./webui/api.js";
+import apiRouter from "../../webui/api.js";
 import cors from "cors";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static("result/dist"));
+app.use(express.static("webui/dist"));
 app.use("/api", apiRouter);
-app.use("/nixosConfigurations", express.static("./webui/nixosConfigurations"));
+app.use("/nixosConfigurations", express.static("../../webui/nixosConfigurations"));
 
 app.listen(8081);
