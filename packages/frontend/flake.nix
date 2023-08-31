ea25a6d395599815fba3bb91d194e09ffb829fa4
tupakkatapa <jesse@ponkila.com>
2023-08-31 19:25:09 +0300

    frontend

---
+++ packages/frontend/flake.nix
@@ -0,0 +1,81 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-root.url = "github:srid/flake-root";
+    backend.url = "github:ponkila/HomestakerOS/feat/pkgs-webui?dir=packages/backend";
+  };
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-parts,
+    backend,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        #"aarch64-darwin"
+        #"aarch64-linux"
+        #"x86_64-darwin"
+        "x86_64-linux"
+      ];
+      imports = [
+        inputs.flake-root.flakeModule
+      ];
+
+      perSystem = {
+        pkgs,
+        lib,
+        config,
+        self',
+        inputs',
+        system,
+        ...
+      }: {
+        formatter = nixpkgs.legacyPackages.${system}.alejandra;
+
+        packages.webui = let
+          src = ./.;
+          version = "0.0.1";
+          name = "homestakeros-webui";
+
+          yarnOfflineCache = pkgs.fetchYarnDeps {
+            yarnLock = "${src}/yarn.lock";
+            hash = "sha256-mkcsTfcCFa+KBct3Btu0S10Pt+QgkZ5vrI0ets8GAxg=";
+          };
+        in
+          pkgs.stdenv.mkDerivation {
+            inherit src version name yarnOfflineCache;
+
+            buildInputs = [
+              pkgs.nodejs_18
+              pkgs.yarn
+              pkgs.yarn2nix-moretea.fixup_yarn_lock
+              inputs'.backend.packages.default
+            ];
+
+            configurePhase = ''
+              export HOME=$(mktemp -d)
+            '';
+
+            buildPhase = ''
+              yarn config --offline set yarn-offline-mirror ${yarnOfflineCache}
+              fixup_yarn_lock yarn.lock
+
+              yarn install --offline
+              patchShebangs .
+
+              yarn --offline build
+            '';
+
+            installPhase = ''
+              mkdir -p $out
+              cp -R webui/dist $out
+            '';
+          };
+
+        packages.default = self'.packages.webui;
+      };
+    };
+}
