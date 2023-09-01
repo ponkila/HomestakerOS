d9bd76ad00ad003be4640c6aa26f7df640ce6da4
tupakkatapa <jesse@ponkila.com>
2023-09-01 03:19:26 +0300

    rm commented out systems

--- packages/backend/flake.nix
+++ packages/backend/flake.nix
@@ -15,9 +15,6 @@
   }:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = [
-        #"aarch64-darwin"
-        #"aarch64-linux"
-        #"x86_64-darwin"
         "x86_64-linux"
       ];
       imports = [
--- packages/frontend/flake.nix
+++ packages/frontend/flake.nix
@@ -13,9 +13,6 @@
   }:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = [
-        #"aarch64-darwin"
-        #"aarch64-linux"
-        #"x86_64-darwin"
         "x86_64-linux"
       ];
       imports = [
